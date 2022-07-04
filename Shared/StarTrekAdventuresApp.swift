023232ac8172ab75950b248a7ce5cd61966ba981
James Emselle <james.e.0111.2@gmail.com>
2022-07-05 08:46:19 +1000

    Initial Commit

---
+++ Shared/StarTrekAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarTrekAdventuresApp.swift
+//  Shared
+//
+//  Created by James Emselle on 5/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct StarTrekAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
